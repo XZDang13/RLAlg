b81932ba7480088908d43a47e28e897af0e8b343
XZDang13 <xdang13@outlook.com>
2024-05-27 14:51:13 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup, find_packages
 
 # Read the long description from the README file
-#with open("README.md", "r", encoding="utf-8") as fh:
-#    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="RLAlg",
     version="0.1.0",
-    author="XDang",
+    author="X Dang",
     author_email="xdang13@outlook.com",
-    description="RL Algorithm in PyTorch",
-    #long_description=long_description,
-    #long_description_content_type="text/markdown",
+    description="A brief description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/XZDang13/RLAlg.git",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.6',
     install_requires=[
         # List your package dependencies here
     ],
