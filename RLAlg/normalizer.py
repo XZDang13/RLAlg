8887920539bdc5e252a4253a06ae5eac36d4220a
Xuzhe Dang <xdang13@outlook.com>
2025-11-18 16:39:11 +0100

    update normalizer

--- RLAlg/normalizer.py
+++ RLAlg/normalizer.py
@@ -1,77 +1,33 @@
 import torch
+import torch.nn as nn
 
-class Normalizer:
-    def __init__(self, size, eps=1e-8, default_clip_range=float('inf'), device="cpu"):
-        self.size = size
-        self.eps = eps
-        self.default_clip_range = default_clip_range
-        # some local information
-        self.local_sum = torch.zeros(self.size).to(device)
-        self.local_sumsq = torch.zeros(self.size).to(device)
-        self.local_count = torch.zeros(1).to(device)
-        # get the total sum sumsq and sum count
-        self.total_sum = torch.zeros(self.size).to(device)
-        self.total_sumsq = torch.zeros(self.size).to(device)
-        self.total_count = torch.ones(1).to(device)
-        # get the mean and std
-        self.mean = torch.zeros(self.size).to(device)
-        self.std = torch.ones(self.size).to(device)
-
-    def normalize(self, v, clip_range=None, update=False):
-        if update:
-            v = v.view(-1, self.size)
-            # do the computing
-            self.local_sum += v.sum(dim=0)
-            self.local_sumsq += (v ** 2).sum(dim=0)
-            self.local_count[0] += v.size(0)
-
-        if clip_range is None:
-            clip_range = self.default_clip_range
-        return torch.clamp((v - self.mean) / (self.std), -clip_range, clip_range)
-    
-    def reset(self):
-        self.local_count.fill_(0)
-        self.local_sum.fill_(0)
-        self.local_sumsq.fill_(0)
-
-    def recompute_stats(self):
-        local_count = self.local_count.clone()
-        local_sum = self.local_sum.clone()
-        local_sumsq = self.local_sumsq.clone()
+class Normalizer(nn.Module):
+    def __init__(self, dim:tuple[int]=()):
+        super().__init__()
         
-        self.local_count.fill_(0)
-        self.local_sum.fill_(0)
-        self.local_sumsq.fill_(0)
-        # sync the stats
-        # update the total stuff
-        self.total_sum += local_sum
-        self.total_sumsq += local_sumsq
-        self.total_count += local_count
-        # calculate the new mean and std
-        self.mean = self.total_sum / self.total_count
-        eps_tensor = torch.tensor(self.eps**2)
-        self.std = torch.sqrt(torch.maximum(eps_tensor, (self.total_sumsq / self.total_count) - (self.total_sum / self.total_count)**2))
+        self.epsilon = 1e-8
+        self.register_buffer("mean", torch.zeros(dim))
+        self.register_buffer("var", torch.ones(dim))
+        self.register_buffer("count", torch.ones(1))
 
-    def state_dict(self):
-        state_dict = {
-            "local_sum": self.local_sum,
-            "local_sumsq": self.local_sumsq,
-            "local_count": self.local_count,
-            "total_sum": self.total_sum,
-            "total_sumsq": self.total_sumsq,
-            "total_count": self.total_count,
-            "mean": self.mean,
-            "std": self.std
-        }
+    def update(self, x:torch.Tensor):
+        batch_mean = torch.mean(x, dim=0)
+        batch_var = torch.var(x, dim=0)
+        batch_count = x.size(0)
 
-        return state_dict
-    
-    def load_state_dict(self, state_dict):
-        self.local_sum = state_dict["local_sum"]
-        self.local_sumsq = state_dict["local_sumsq"]
-        self.local_count = state_dict["local_count"]
-        self.total_sum = state_dict["total_sum"]
-        self.total_sumsq = state_dict["total_sumsq"]
-        self.total_count = state_dict["total_count"]
-        self.mean = state_dict["mean"]
-        self.std = state_dict["std"]
+        delta = batch_mean - self.mean
+        total_count = self.count + batch_count
+
+        m_a = self.var * self.count
+        m_b = batch_var * batch_count
+        M2 = m_a + m_b + torch.square(delta) * self.count * batch_count / total_count
+
+        self.mean = self.mean + delta * batch_count / total_count
+        self.var = M2 / total_count
+        self.count = total_count
+
+    def forward(self, x:torch.Tensor, update:bool=False):
+        if update:
+            self.update(x)
+
+        return (x - self.mean) / (torch.sqrt(self.var) + self.epsilon)
